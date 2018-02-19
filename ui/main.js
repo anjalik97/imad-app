9fdfb81c75fcdd5253b7fe834b209f31a539ccde
anjalik97 <36513328+anjalik97@users.noreply.github.com>
2018-02-19 14:39:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element=getElementById('main-text');
-element.innerHTML="new value";
+element.innerHTML='new value';
 var img=document.getElementById('madi');
 img.onclick=function()
 {img.style.margin='100px';};
